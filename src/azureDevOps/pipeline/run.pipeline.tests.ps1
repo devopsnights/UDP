8a64f24805b2eab48472be48ac58e6c2a2d4dcdc
Wesley Camargo <camargo.wes@gmail.com>
2021-10-08 10:56:42 +0200

    timeout parametrized

--- src/azureDevOps/pipeline/run.pipeline.tests.ps1
+++ src/azureDevOps/pipeline/run.pipeline.tests.ps1
@@ -3,10 +3,11 @@ param(
     [string[]]$TestScripts
 )
 
-Write-Host "Test scripts: $TestScripts"
+Write-Host "Test scripts: $env:testFilesToRun"
 
 # Create configuration for pester execution
-$container = New-PesterContainer -Path $TestScripts 
+# $container = New-PesterContainer -Path $TestScripts 
+$container = New-PesterContainer -Path $env:testFilesToRun
 
 $config = New-PesterConfiguration
 $config.TestResult.Enabled = $true
