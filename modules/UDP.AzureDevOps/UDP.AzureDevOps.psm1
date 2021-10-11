f6f310a9a3c6495275308b16089d7a61abe49a07
Wesley Camargo <camargo.wes@gmail.com>
2021-10-11 18:01:46 +0200

    app config implementation

--- modules/UDP.AzureDevOps/UDP.AzureDevOps.psm1
+++ modules/UDP.AzureDevOps/UDP.AzureDevOps.psm1
@@ -102,18 +102,11 @@ function New-AzureDevOpsPipeline {
 
     Write-Host "##[command]Creating pipeline '$pipelineName'"
 
-    Write-Host "Source Branch: $env:Build_SourceBranch"
-    Write-Host "System_PullRequest_SourceBranch: $env:System_PullRequest_SourceBranch"
-
-    Get-ChildItem -Path Env:
-
     if($env:System_PullRequest_pullRequestId){
         $branchName = $env:System_PullRequest_SourceBranch
     }else{
         $branchName = $env:Build_SourceBranch
     }
-
-  
     
     $pipeline = az pipelines create `
         --name $pipelineName `
